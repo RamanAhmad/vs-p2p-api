5b1d34c74a32a864533b0ffe56aa2a5fb7b821e0
SimSon <s-schwarzkopf+git@etik.com>
2024-12-10 17:34:37 +0100

    Refactor RegisterTestCreator to use TestCase class

--- testcases/creators/system/RegisterTestCreator.py
+++ testcases/creators/system/RegisterTestCreator.py
@@ -1,7 +1,6 @@
+from creators.TestCase import TestCase
 from ..BaseTestCreator import BaseTestCreator
 from typing import Type
-from ColumnName import ColumnName
-from star.Component import Component
 
 
 # This class is responsible for creating the test cases for the POST register component endpoint
@@ -11,23 +10,19 @@ class RegisterTestCreator(BaseTestCreator):
     
     def __init__(self, generator: Type['TestGenerator']): # type: ignore
         super().__init__(generator)
-        self.__new_component: Component = self.get_components()[1]
         
 
-    def get_test_cases(self) -> list[list]:
+    def _init_test_cases(self) -> list[list]:
         star_uuid = self.get_star_uuid()
-        sol_uuid = self.get_sol_uuid()
-        status = 200
-        com_uuid = self.__new_component.get_uuid()
-        com_ip = self.__new_component.get_ip()
-        com_port = self.__new_component.get_port()
-        
+        sol = self.get_sol()
+        com = self.get_components()[1]
+
         test_cases = [
-            [com_uuid, star_uuid, sol_uuid, com_uuid, com_ip, com_port, status, 200, "Only valid json data"],
-            [com_uuid, star_uuid, sol_uuid, 4711, com_ip, com_port, status, 409, "Mismatching COM_UUID in json data"],
-            [com_uuid, star_uuid, sol_uuid, com_uuid, "1.1.1.1", com_port, status, 409, "Mismatching IP for given COM_UUID"],
-            [com_uuid, star_uuid, sol_uuid, com_uuid, com_ip, com_port, 401, 409, "Status in json is not 200"],
-            [com_uuid, "ABCD", sol_uuid, com_uuid, com_ip, com_port, status, 401, "Unknown STAR_UUID in json data"],
-            [com_uuid, star_uuid, 4711, com_uuid, com_ip, com_port, status, 401, "Mismatching SOL_UUID in json data"],
+            TestCase(test_name=self.get_test_name(), case_desc="Only valid json data", expected_status=200, star_uuid=star_uuid, sol=sol, com_2=com, status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=self.get_test_name(), case_desc="Mismatching COM_UUID in json data", expected_status=409, star_uuid=star_uuid, sol=sol, com_2=com.get_modified_clone(new_uuid=4711), status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=self.get_test_name(), case_desc="Mismatching IP for given COM_UUID", expected_status=409, star_uuid=star_uuid, sol=sol, com_2=com.get_modified_clone(new_ip="4.7.1.1"), status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=self.get_test_name(), case_desc="Status in json is not 200", expected_status=409, star_uuid=star_uuid, sol=sol, com_2=com, status=500, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=self.get_test_name(), case_desc="Unknown STAR_UUID in json", expected_status=401, star_uuid="ABCD", sol=sol, com_2=com, status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=self.get_test_name(), case_desc="Mismatching SOL_UUID in json", expected_status=200, star_uuid=star_uuid, sol=sol.get_modified_clone(new_uuid=4711), com_2=com, status=200, base_host=sol.get_ip(), base_port=sol.get_port())
         ]
-        return test_cases
+        self.add_test_cases(test_cases)
