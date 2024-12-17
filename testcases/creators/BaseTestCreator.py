1338020fc8c6f3c959162c659054189bebbf7316
SimSon <s-schwarzkopf+git@etik.com>
2024-12-17 11:38:40 +0100

    Update FAKE_STATUS constant

--- testcases/creators/BaseTestCreator.py
+++ testcases/creators/BaseTestCreator.py
@@ -7,7 +7,7 @@ class BaseTestCreator(ABC):
     FAKE_IP = "4.7.1.1"
     FAKE_PORT = "4711"
     FAKE_UUID = "4711"
-    FAKE_STATUS = "200"
+    FAKE_STATUS = "500"
     FAKE_STAR_UUID = "ABCD"
     
     def __init__(self, generator: Type['TestGenerator']): # type: ignore
