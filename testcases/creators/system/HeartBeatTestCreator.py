370b6bfbed6eb32cf5586bd4a41f2bbb7fe5679f
SimSon <s-schwarzkopf+git@etik.com>
2024-12-17 09:47:31 +0100

    FIX: wrong naming of component variable

--- testcases/creators/system/HeartBeatTestCreator.py
+++ testcases/creators/system/HeartBeatTestCreator.py
@@ -19,12 +19,12 @@ class HeartBeatTestCreator(BaseTestCreator):
         name = self.get_test_name()
         
         test_cases = [
-            TestCase(test_name=name, case_desc="Only valid json data", expected_status=200, star_uuid=star_uuid, sol=sol, com_self=com, com_path=com_self.get_uuid() , status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=name, case_desc="Only valid json data", expected_status=200, star_uuid=star_uuid, sol=sol, com_self=com, com_path=com.get_uuid() , status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
             TestCase(test_name=name, case_desc="Unknown COM_UUID in path", expected_status=404, star_uuid=star_uuid, sol=sol, com_self=com, com_path=4711 , status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
-            TestCase(test_name=name, case_desc="Mismatching COM_UUID in json path", expected_status=409, star_uuid=star_uuid, sol=sol, com_self=com.get_modified_clone(new_uuid=4711), com_path=com_self.get_uuid(), status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
-            TestCase(test_name=name, case_desc="Mismatching IP in json path", expected_status=409, star_uuid=star_uuid, sol=sol, com_self=com.get_modified_clone(new_ip="4.7.1.1"), com_path=com_self.get_uuid(), status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
-            TestCase(test_name=name, case_desc="Status in json is not 200", expected_status=409, star_uuid=star_uuid, sol=sol, com_self=com, com_path=com_self.get_uuid(), status=505, base_host=sol.get_ip(), base_port=sol.get_port()),
-            TestCase(test_name=name, case_desc="Unknown STAR_UUD in json data", expected_status=401, star_uuid="ABCD", sol=sol, com_self=com, com_path=com_self.get_uuid(), status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
-            TestCase(test_name=name, case_desc="Mismatching SOL_UUID in json data", expected_status=401, star_uuid=star_uuid, sol=sol.get_modified_clone(new_uuid=4711), com_self=com, com_path=com_self.get_uuid(), status=200, base_host=sol.get_ip(), base_port=sol.get_port())
+            TestCase(test_name=name, case_desc="Mismatching COM_UUID in json path", expected_status=409, star_uuid=star_uuid, sol=sol, com_self=com.get_modified_clone(new_uuid=4711), com_path=com.get_uuid(), status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=name, case_desc="Mismatching IP in json path", expected_status=409, star_uuid=star_uuid, sol=sol, com_self=com.get_modified_clone(new_ip="4.7.1.1"), com_path=com.get_uuid(), status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=name, case_desc="Status in json is not 200", expected_status=409, star_uuid=star_uuid, sol=sol, com_self=com, com_path=com.get_uuid(), status=505, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=name, case_desc="Unknown STAR_UUD in json data", expected_status=401, star_uuid="ABCD", sol=sol, com_self=com, com_path=com.get_uuid(), status=200, base_host=sol.get_ip(), base_port=sol.get_port()),
+            TestCase(test_name=name, case_desc="Mismatching SOL_UUID in json data", expected_status=401, star_uuid=star_uuid, sol=sol.get_modified_clone(new_uuid=4711), com_self=com, com_path=com.get_uuid(), status=200, base_host=sol.get_ip(), base_port=sol.get_port())
         ]
         self.add_test_cases(test_cases)
